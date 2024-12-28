530b9235001d9c5ab2eccdc26b27077eaca0a124
Felix Ogundare <felix.ogundare@wemabank.com>
2024-12-28 20:01:26 +0100

    clear comment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,19 +2,6 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { GET } from "@/app/api/cards/route";
-import { GETINVOICES } from "@/app/api/invoices/route";
-import { GETREVENUE } from "@/app/api/revenue/route";
-import {
-  CustomerField,
-  CustomersTableType,
-  Cards,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoice,
-  LatestInvoiceRaw,
-  Revenue,
-} from "@/app/lib/definitions";
 import {
   fetchRevenue,
   fetchLatestInvoices,
